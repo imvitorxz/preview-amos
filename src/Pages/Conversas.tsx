a6e1608ca26bb1efb992ea6c032a6357551a6ffe
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-16 19:21:43 -0300

    feat: terminando página conversas

--- src/Pages/Conversas.tsx
+++ src/Pages/Conversas.tsx
@@ -1,7 +1,110 @@
+import styles from './Conversas.module.css';
+
 export default function Conversas() {
     return (
-        <div>
+        <div className={styles.divChats}>
             <h1>Conversas</h1>
+            <div className={styles.divChatsModules}>
+                <div className={styles.divChatsModule}>
+                    <h2>Por pastas</h2>
+                    <div className={styles.divChatsModuleFolders}>
+                        <div className={styles.divChatsModuleFoldersItem}>
+                            <h3>Amigos</h3>
+                            <div className={styles.divChatsModuleFoldersItemContent}>
+                                <p>3 conversas</p>
+                                
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Como socializar mais?</p>
+                                <p>Hoje</p>
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Como lidar com conflitos?</p>
+                                <p>Ontem</p>
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Planejando uma viagem</p>
+                                <p>Semana passada</p>
+                            </div>
+                        </div>
+                        <div className={styles.divChatsModuleFoldersItem}>
+                            <h3>Família</h3>
+                            <div className={styles.divChatsModuleFoldersItemContent}>
+                                <p>2 conversas</p>
+                                
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Como lidar com a perda?</p>
+                                <p>Hoje</p>
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Planejando uma reunião</p>
+                                <p>Semana passada</p>
+                            </div>
+                        </div>
+                        <div className={styles.divChatsModuleFoldersItem}>
+                            <h3>Trabalho</h3>
+                            <div className={styles.divChatsModuleFoldersItemContent}>
+                                <p>1 conversa</p>
+                                
+                            </div>
+                            <div className={styles.divChatsModuleFoldersItemContentChat}>
+                                <p>Como lidar com a pressão?</p>
+                                <p>Hoje</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.divChatsModule}>
+                    <h2>Por data</h2>
+                    <div className={styles.divChatsModuleDate}>
+                        <div className={styles.divChatsModuleDateItem}>
+                            <h3>Hoje</h3>
+                            <div className={styles.divChatsModuleDateItemContent}>
+                                <p>3 conversas</p>
+                                
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Como socializar mais?</p>
+                                <p>Hoje</p>
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Como lidar com conflitos?</p>
+                                <p>Ontem</p>
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Planejando uma viagem</p>
+                                <p>Semana passada</p>
+                            </div>
+                        </div>
+                        <div className={styles.divChatsModuleDateItem}>
+                            <h3>Ontem</h3>
+                            <div className={styles.divChatsModuleDateItemContent}>
+                                <p>2 conversas</p>
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Como lidar com conflitos?</p>
+                                <p>Ontem</p>
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Planejando uma viagem</p>
+                                <p>Semana passada</p>
+                            </div>
+                        </div>
+                        <div className={styles.divChatsModuleDateItem}>
+                            <h3>Semana passada</h3>
+                            <div className={styles.divChatsModuleDateItemContent}>
+                                <p>1 conversa</p>
+                                
+                            </div>
+                            <div className={styles.divChatsModuleDateItemContentChat}>
+                                <p>Planejando uma viagem</p>
+                                <p>Semana passada</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Pages/Sobre.tsx
+++ src/Pages/Sobre.tsx
@@ -2,6 +2,7 @@ export default function Sobre() {
     return (
         <div>
             <h1>Sobre</h1>
+            <p>PS: a imagem da menina eu peguei no canva! todos os créditos vão pra ele</p>
         </div>
     )
 }
