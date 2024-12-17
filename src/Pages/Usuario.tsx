3f129ab7c6024734eae9fbb2838bf6b4cdee5ebf
Vitor Colombelli <vittorcolom@gmail.com>
2024-12-16 21:01:21 -0300

    feat: começando a página usuarios

--- src/Pages/Usuario.tsx
+++ src/Pages/Usuario.tsx
@@ -1,7 +1,23 @@
+import styles from './Usuario.module.css';
+
+import Img_User from '../assets/icons/icon_user.png';
+
 export default function Usuario() {
     return (
-        <div>
-            <h1>Usuario</h1>
+        <div className={styles.divUsuario}>
+            <img src={Img_User} className={styles.imgPerfil} alt="Icone usuario"/>
+            <h2>Usuário</h2>
+            <h3>Nome</h3>
+            <h2>Senha</h2>
+            <input id="inputUsuarioSenha" name="inputUsuarioSenha" type="password"/>
+            <h2>Bio</h2>
+            <input id={styles.inputUsuarioBio} name="inputUsuarioBio" type="text"/>
+            <h2>Objetivos</h2>
+            <ul>
+                <li>Objetivo 1</li>
+                <li>Objetivo 2</li>
+                <li>Objetivo 3</li>
+            </ul>
         </div>
     )
 }
