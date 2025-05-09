2b92341fbcd29705d4ac2a752830bf69cb2db692
Vitor Colombelli <vittorcolom@gmail.com>
2025-05-09 19:01:11 -0300

    feat: subindo demo

--- src/Components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import './Footer.module.css';
-
-export default function Footer() {
-    return (
-        <footer>
-            <p>© 2024 - <span>AMÓS</span> - desenvolvido por Vitor de Oliveira</p>
-        </footer>
-    )
-}
